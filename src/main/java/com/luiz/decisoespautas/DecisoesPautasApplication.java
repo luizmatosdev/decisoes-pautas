258c263ed53822ebbabbf19e0867177a49be0cdd
Luiz <luizmatosedu@gmail.com>
2024-06-12 16:01:52 -0300

    🎉 Commit inicial

---
+++ src/main/java/com/luiz/decisoespautas/DecisoesPautasApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.decisoespautas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisoesPautasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecisoesPautasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luiz/decisoespautas/DecisoesPautasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.decisoespautas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisoesPautasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
